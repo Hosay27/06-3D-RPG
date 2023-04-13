13c02a68cd01e9d492850cf32212bb9416f4fdf7
Hosay27 <jombauti@iu.edu>
2023-04-13 13:21:55 -0400

    test

--- NPC/NPC.gd
+++ NPC/NPC.gd
@@ -1,36 +1,8 @@
 extends KinematicBody
 
-onready var Dialogue = get_node("/root/Game/UI/Dialouge")
-var time = 1
-var time1 = 0
-
-var dialogue = [
-	"Hello",
-	"How are you?",
-	"You're Gay",
-	"asdaskdljaldjlkajwdpqwiuhghvcbqxwxklqwjxjqwkld qwjdkljkqldjqkldjqd qdqkjldjqldjqldaodx"
-]
-
-func _ready():
-	Dialogue.connect("finished_dialouge", self,"finished")
-	$AnimationTree.set("parameters/Idle_Change/blend_amount", 0)
-	$Timer.start()
+var dialogue = Dialogic.start("Npc")
 
 func _on_Area_body_entered(body):
 	if body.name == "Player":
-		Dialogue.start_dialouge(dialogue)
-
-func _on_Area_body_exited(body):
-	if body.name == "Player":
-		Dialogue.hide_dialouge()
-
-func finished():
-	queue_free()
-
-func _on_Timer_timeout():
-	time -= .1
-	$AnimationTree.set("parameters/Idle_Change/blend_amount", 1)
-	$Timer2.start()
-
-func _on_Timer2_timeout():
-	$AnimationTree.set("parameters/Idle_Change/blend_amount", 0)
+		var dialogue = Dialogic.start("Npc")
+		add_child(dialogue)
